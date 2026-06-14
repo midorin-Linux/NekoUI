e1006fd35ef1ecc2b0b7238c5a52d64a4da4ed20
midorin-Linux <midorin.shak12@gmail.com>
2026-06-14 22:19:40 +0900

    Add SecretKey struct with serialization and deserialization implementations

--- nekoui/src/utils/secret_key.rs
+++ nekoui/src/utils/secret_key.rs
@@ -1,6 +1,8 @@
-use std::fmt as stdfmt;
+use std::{fmt as stdfmt, fmt};
 
 use chrono::Local;
+use secrecy::{ExposeSecret, SecretString};
+use serde::{Deserialize, Serialize};
 use tracing::{
     Event, Subscriber,
     field::{Field, Visit},
@@ -9,8 +11,59 @@ use tracing_subscriber::{
     fmt::{FmtContext, FormatEvent, FormatFields, format::Writer},
     registry::LookupSpan,
 };
+use zeroize::Zeroizing;
 
 const MAX_FIELD_VALUE_LEN: usize = 100;
+
+#[derive(Clone)]
+pub struct SecretKey(Zeroizing<SecretString>);
+
+impl SecretKey {
+    pub fn new(value: String) -> Self {
+        Self(Zeroizing::new(SecretString::new(value.into())))
+    }
+
+    pub fn expose(&self) -> &str {
+        (*self.0).expose_secret()
+    }
+}
+
+impl AsRef<str> for SecretKey {
+    fn as_ref(&self) -> &str {
+        self.expose()
+    }
+}
+
+impl Serialize for SecretKey {
+    fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        serializer.serialize_str(self.expose())
+    }
+}
+
+impl<'de> Deserialize<'de> for SecretKey {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let s = String::deserialize(deserializer)?;
+        Ok(Self::new(s))
+    }
+}
+
+impl fmt::Debug for SecretKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        let visible_length = 4;
+        let masked = {
+            let inner = (*self.0).expose_secret();
+            let length = inner.chars().count();
+            let start = length.saturating_sub(visible_length);
+            let extracted: String = inner.chars().skip(start).collect();
+            format!("{:*>20}", &extracted)
+        };
+        f.debug_tuple("SecretKey").field(&masked).finish()
+    }
+}
+
+impl Drop for SecretKey {
+    fn drop(&mut self) {}
+}
 
 pub struct TruncatingEventFormat;
 
